110f371204741cb8efc59651ce96c3724ae78a02
LM Yudhy Prayitno <119650090+Yudhy-code-stranger@users.noreply.github.com>
2023-11-19 11:41:30 +0800

    commit awal

---
+++ assistant.py
@@ -0,0 +1,3 @@
+import openai
+
+openai.api_key = str(open("token.txt").read())
---
+++ chat.py
@@ -0,0 +1,14 @@
+# # import the library
+import flet as ft
+
+# defining the main function
+def main(page:ft.Page):
+    # setting the page title
+    page.title = "Hello World!"
+    
+    # text control
+    text= ft.Text(value='My first app with flutter', color="blue")
+    page.controls.append(text)
+    page.update()
+# starting the app
+ft.app(target=main, view = ft.WEB_BROWSER)
