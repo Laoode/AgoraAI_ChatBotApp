2a9e9cc8937b7e1d96c639798290bc828539f235
Alvian Ardiansyah <alvianardiansyah.22039@gmail.com>
2023-11-19 12:51:20 +0800

    keterangan

--- chat.py
+++ chat.py
@@ -6,8 +6,9 @@ def main(page:ft.Page):
     # setting the page title
     page.title = "Hello World!"
     
+    
     # text control
-    text= ft.Text(value='My first app with flutter', color="blue")
+    text= ft.Text(value='kasian kamu hang', color="blue")
     page.controls.append(text)
     page.update()
 # starting the app
