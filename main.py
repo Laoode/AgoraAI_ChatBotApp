982bee7c205cd9a5ff9540dc5cd6750764d4c938
unknown <mahdimadin8@gmail.com>
2023-11-23 18:34:46 +0800

    function build

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
 # # import the library
 import flet as ft
 
+# Madin
+    def build(self):
+        return ft.Container(
+            width=320,
+            height=40,
+            border=ft.border.only(bottom=ft.border.BorderSide(0.5, "white45")),
+            content=ft.Row(
+                spacing=20,
+                vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                controls=[
+                    ft.Icon(
+                        name=self.icon_name,
+                        size=14,
+                        opacity=0.85,
+                    ),
+                    ft.TextField(
+                        border_color="transparent",
+                        bgcolor="transparent",
+                        height=20,
+                        width=180,
+                        text_size=12,
+                        content_padding=3,
+                        cursor_color="white",
+                        hint_text=self.text_hint,
+                        hint_style=ft.TextStyle(size=11),
+                        password=self.hide,
+                        on_change=lambda e: self.get_prefix_emails(e),
+                        on_blur=lambda e: self.get_green_check(e),
+                    ),
+                    self.prefix_email_containers(),
+                    self.off_focus_input_check(),
+                ],
+            )
+        )
+
 # defining the main function
 def main(page:ft.Page):
     # setting the page title
