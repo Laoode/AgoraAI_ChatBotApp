3fc2958259035981f45a620841bc6e3ee441989b
LM Yudhy Prayitno <119650090+Yudhy-code-stranger@users.noreply.github.com>
2023-11-24 02:37:14 +0800

    fix api

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# # import the library
-# Abdul fath ramadhan start
+# import the library
 import flet as ft
 from flet import *
 from math import pi
@@ -7,17 +6,16 @@ import time
 import threading
 import openai
 
-openai.api_key="sk-87kg0SZNUQV5GJWUQztwT3BIbkFJyOWFVMDTDxsw9mM5lw0G"
+openai.api_key="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"
 def main_style():
    #the styling properties for the main content area class
    return {
        "width": 420,
        "height": 450,
-       "bgcolor": "abeefa",
+       "bgcolor": "ebeefa",
        "border_radius": 10,
        "padding": 15,
    } 
-# Abdul fath ramadhan end
 
 # defining the main function
 def main(page:ft.Page):
