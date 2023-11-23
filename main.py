72a21218311291dfdf431e690a66d999dd071d54
ade sulastri <adesulastri878@gmail.com>
2023-11-23 18:36:50 +0800

    prompt style ade

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 # # import the library
 import flet as ft
 
-# defining the main function
-def main(page:ft.Page):
-    # setting the page title
-    page.title = "Kelompok1.chatbot.ai"
-    
+# ADE start
+# Style 
+def prompt_style():
+  return {
+    "width": 420,
+    "height": 40,
+    "border_color": "white",
+    "content_padding": 10,
+    "cursor_color": "white",
+  }
 
+#main content area atau bagian home dari chatbot
+class MainContentArea(ft.Container):
+  def __init__(self):
+    #super() merupakan inheritance atau pewarisan metode untuk mengambil atribut dari class parent
+    super().__init__(**main_style())
+    self.chat = ft.ListView(
+      expand=True,
+      height=200,
+      spacing=15,
+      auto_scroll=True,
+    )
 
-    # text control
-    text= ft.Text(value='Testing Home', color="blue")
-    page.controls.append(text)
-    page.update()
-# starting the app
-ft.app(target=main, view = ft.WEB_BROWSER)
+    self.content = self.chat
+# ADE end
