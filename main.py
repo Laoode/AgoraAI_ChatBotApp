35faf13fedeb0dfe755566036920c0d811383fd9
revaputrikr <girlwithlcv@gmail.com>
2023-11-23 18:18:02 +0800

    add tab2_content

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # # import the library
 import flet as ft
 
+def tab2_content():
+    main_area = MainContentArea()
+    prompt = Prompt(chat=main_area.chat)
+    return ft.Column([
+        main_area,
+        ft.Divider(height=3, color="transparent"),
+        prompt,
+    ])
+def tab3_content():
+    return ft.Text("This is Tab 3 Content", size=18, weight="w800")
+
 # defining the main function
 def main(page:ft.Page):
     # setting the page title
