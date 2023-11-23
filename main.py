f6ee73177582b1aa7619dc77dff6be13b822253c
unknown <raynharddharmawan10@gmail.com>
2023-11-23 18:21:50 +0800

    page navbar bagian akhir

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 # # import the library
 import flet as ft
 
+
+    navbar = ft.Container(
+        **navbar_style(),
+        content=ft.Column([
+            ft.Row([
+                ft.IconButton(icon="menu", icon_size=25, icon_color="white"),
+                ft.Text("Agora-AI", size=25, color="white", weight="bold"),
+                ft.Row([
+                    ft.IconButton(icon="notifications", icon_size=25, icon_color="white"),
+                    ft.IconButton(icon="search", icon_size=25, icon_color="white"),
+                ])
+            ], alignment="spaceBetween"),
+            t
+        ])
+    )
+
+    page.add(navbar)
+    page.update()
+
+if __name__ == "__main__":
+    ft.app(target=main)
+    
 # defining the main function
 def main(page:ft.Page):
     # setting the page title
