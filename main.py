4a78456c31a3435269e0df5445782a553255cf7d
niwayanerdiani <niwayanerdianizzz@gmail.com>
2023-11-23 17:27:17 +0700

    runprompt

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 # # import the library
 import flet as ft
+import openai 
+
+
+# start wayan 
+# method : run all methods when started
+def run_prompt(self, event):
+    #set the value of the user prompt
+    text = event.control.value
+
+    # disabling input field can also be added ...
+    self.value = ""
+    self.update()
+
+    # first, we output the user prompt
+    self.user_output(prompt=text)
+
+    # second, we display GPT output
+    self.gpt_output(prompt=text)
+# end wayan 
 
 # defining the main function
 def main(page:ft.Page):
