35973ebcc68006d530419245ac43f8a2bc29007a
unknown <rahmi.22027@gmail.com>
2023-11-23 18:30:54 +0800

    add CreateMessage

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 # # import the library
 import flet as ft
 
+#Rahmiii start
+#before pushing text to UI - create a class that generates the UI for the actual text prompts
+class CreateMessage(ft.Column):
+    def __init__(self, name: str, message: str):
+        self.name = name #show's which prompt is whos
+        self.message = message
+        self.text= ft.Text(self.message)
+        super().__init__(spacing=4)
+        self.controls=[ft.Text(self.name, opacity=0.6), self.text]
+
+#user input class
+class Prompt(ft.TextField):
+    def __init__(self, chat:ft.ListView):
+        super().__init__(**prompt_style(), on_submit=self.run_prompt)
+        #need access to main chat area -from MainContentArea class
+        self.chat: ft.ListView =chat
+#Rahmi end
+
 # defining the main function
 def main(page:ft.Page):
     # setting the page title
