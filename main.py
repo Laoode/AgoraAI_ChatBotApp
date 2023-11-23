a4076ccc5a9f8d68d0d69c6388948086f6fe5249
Alvin Saputra <laodealvinrahmats@gmail.com>
2023-11-23 18:22:13 +0800

    userInput

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 # # import the library
 import flet as ft
 
+class userInputField(ft.UserControl):
+    def__init__(self,icon_name,text_hint,hide,function,emails:bool, function_check:bool):
+        self.icon_name = icon_name
+        self.text_hint = text_hint
+        self.hide = hide
+        self.function_emails = function_emails
+        seld.function_check = function_check
+        super().__init()
+    
+    def return_email_prefix(self, e):
+        email = self.controls[0].content.controls[1].value
+        if e.control.data in email:
+            pass
+        else:
+            self.controls[0].content.controls[1].value+= e.control.data
+            self.controls[0].content.controls[2].offset = ft.transform.Offset(0.5, 0)
+            self.controls[0].content.controls[2].opacity = 0
+            self.update()
+                    
+
+
+
+
+
+
+
 # defining the main function
 def main(page:ft.Page):
     # setting the page title
