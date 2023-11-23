1f8c8f0fe7358fa0e619900f37bbe88accd8f523
unknown <muhammadbayanudinf@gmail.com>
2023-11-23 19:45:07 +0800

    add input check

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
 # # import the library
 import flet as ft
 
+    def off_focus_input_check(self):
+        return ft.Container(
+            opacity=0,
+            offset=ft.transform.Offset(0, 0),
+            animate=200,
+            border_radius=6,
+            width=18,
+            height=18,
+            alignment=ft.alignment.center,
+            content=ft.Checkbox(
+                fill_color="#7df6dd",
+                check_color="black",
+                disabled=True,
+            )
+        )
+    
+    def get_prefix_emails(self, e):
+        if self.function_emails:
+            email = self.controls[0].content.controls[1].value
+            if e.data:
+                if "@gmail.com" in email or "@hotmail.com" in email:
+                    self.controls[0].content.controls[2].offset = ft.transform.Offset(0, 0)
+                    self.controls[0].content.controls[2].opacity = 0
+                    self.update()
+                else:
+                    self.controls[0].content.controls[2].offset = ft.transform.Offset(-0.15, 0)
+                    self.controls[0].content.controls[2].opacity = 1
+                    self.update()
+            else:
+                self.controls[0].content.controls[2].offset = ft.transform.Offset(0.5, 0)
+                self.controls[0].content.controls[2].opacity = 0
+                self.update()
+        else:
+            pass
+
 # defining the main function
 def main(page:ft.Page):
     # setting the page title
